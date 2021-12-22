5a336a1055422c48dd041a9156e59a0aeaf94ded
xiaoduan.KD <1961018000@qq.com>
2021-12-22 18:11:15 +0800

    feat: 提交榜单模块constants代码

--- src/pages/discover/c-pages/recommend/store/constants.js
+++ src/pages/discover/c-pages/recommend/store/constants.js
@@ -2,6 +2,6 @@ export const CHANGE_TOP_BANNER = 'recommend/CHANGE_TOP_BANNER';
 export const CHANGE_HOT_RECOMMEND = 'recommend/CHANGE_HOT_RECOMMEND';
 export const CHANGE_NEW_ALBUM = 'recommend/CHANGE_NEW_ALBUM';
 
-export const CHANGE_UP_RANKING_LIST= 'recommend/CHANGE_UP_RANKING_LIST';
-export const CHANGE_NEW_SONG_LIST= 'recommend/CHANGE_NEW_SONG_LIST';
-export const CHANGE_ORIGIN_LIST= 'recommend/CHANGE_ORIGIN_LIST';
+export const CHANGE_UP_RANKING_LIST = 'recommend/CHANGE_UP_RANKING_LIST';
+export const CHANGE_NEW_SONG_LIST = 'recommend/CHANGE_NEW_SONG_LIST';
+export const CHANGE_ORIGIN_LIST = 'recommend/CHANGE_ORIGIN_LIST';
